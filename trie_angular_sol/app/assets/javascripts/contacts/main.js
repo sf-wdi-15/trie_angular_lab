02c97371e9f1636decb42ba557c500b1a7947209
Delmer Reed <delmer814@yahoo.com>
2015-02-09 02:38:54 -0800

    first refactor

---
+++ trie_angular_sol/app/assets/javascripts/contacts/main.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+  
+  var ContactsApp = angular.module("ContactsApp", []);
+
+  ContactsApp.config(["$httpProvider", function($httpProvider){
+      $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  }])
+});
